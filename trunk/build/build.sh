5ef178a7a1c95daf11141987c67b4e97e7146bce
aballmer <alexandersballmer@gmail.com>
2015-06-26 18:35:18 +0000

    Some makefile tweaks to build on more systems

--- trunk/build/build.sh
+++ trunk/build/build.sh
@@ -1,3 +1,4 @@
 make clean
 make
-mpicc test.c -O0 -I ~/Documents/argonne/scc15-spack/opt/unknown_arch/gcc@5.1.0/mpich@3.0.4/include -I  ~/Documents/argonne/scc15-spack/opt/unknown_arch/gcc@5.1.0/hwloc@1.9.1/include/ -L  ~/Documents/argonne/scc15-spack/opt/unknown_arch/gcc@5.1.0/hwloc@1.9.1/lib -L ~/Documents/argonne/autoperf-x86/trunk/build -I ~/Documents/argonne/autoperf-x86/trunk/src/lib -lpapi -L/home/gnu3ra/Documents/argonne/scc15-spack/opt/unknown_arch/gcc@5.1.0/papi@5.4.1/lib -I /home/gnu3ra/Documents/argonne/scc15-spack/opt/unknown_arch/gcc@5.1.0/papi@5.4.1/include -lautoperf -lhwloc -luuid -o perftest
+SPACKDIR=/home/aballmer
+mpicc test.c -O0 -I $SPACKDIR/scc15-spack/opt/unknown_arch/gcc@5.1.0/mpich@3.0.4/include -I  $SPACKDIR/scc15-spack/opt/unknown_arch/gcc@5.1.0/hwloc@1.9.1/include/ -L  $SPACKDIR/scc15-spack/opt/unknown_arch/gcc@5.1.0/hwloc@1.9.1/lib -L $SPACKDIR/autoperf-x86/trunk/build -I $SPACKDIR/autoperf-x86/trunk/src/lib -lpapi -L$SPACKDIR/opt/unknown_arch/gcc@5.1.0/papi@5.4.1/lib -I $SPACKDIR/scc15-spack/opt/unknown_arch/gcc@5.1.0/papi@5.4.1/include -lautoperf -lhwloc -luuid -o perftest
