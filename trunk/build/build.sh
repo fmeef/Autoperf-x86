4767e6ca97e92497be75ffcec83ba16fb1e1e6e5
gnu3ra <aballmer42@gmail.com>
2015-06-26 16:21:40 -0500

    Tries to autodetect compiler now

--- trunk/build/build.sh
+++ trunk/build/build.sh
@@ -1,4 +1,5 @@
 make clean
 make
-SPACKDIR=/home/aballmer
-mpicc test.c -O0 -I $SPACKDIR/scc15-spack/opt/unknown_arch/gcc@5.1.0/mpich@3.0.4/include -I  $SPACKDIR/scc15-spack/opt/unknown_arch/gcc@5.1.0/hwloc@1.9.1/include/ -L  $SPACKDIR/scc15-spack/opt/unknown_arch/gcc@5.1.0/hwloc@1.9.1/lib -L $SPACKDIR/autoperf-x86/trunk/build -I $SPACKDIR/autoperf-x86/trunk/src/lib -lpapi -L$SPACKDIR/opt/unknown_arch/gcc@5.1.0/papi@5.4.1/lib -I $SPACKDIR/scc15-spack/opt/unknown_arch/gcc@5.1.0/papi@5.4.1/include -lautoperf -lhwloc -luuid -o perftest
+SPACKDIR=/home/gnu3ra/Documents/argonne/scc15-spack
+GCC=$(gcc --version | grep ^gcc | sed 's/^.* //g')
+mpicc test.c -O0 -I $SPACKDIR/opt/unknown_arch/gcc@$GCC/mpich@3.0.4/include -I  $SPACKDIR/opt/unknown_arch/gcc@$GCC/hwloc@1.9.1/include/ -L  $SPACKDIR/opt/unknown_arch/gcc@$GCC/hwloc@1.9.1/lib -L ../build -I ../src/lib -lpapi -L$SPACKDIR/opt/unknown_arch/gcc@$GCC/papi@5.4.1/lib -I $SPACKDIR/opt/unknown_arch/gcc@5.1.0/papi@5.4.1/include -lautoperf -lhwloc -luuid -o perftest
